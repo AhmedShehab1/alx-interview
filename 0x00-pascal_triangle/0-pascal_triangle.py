cf1c5fccae0c5dfc7f6a3fe316bb712bb1012246
AhmedShehab1 <legendbtw2003@gmail.com>
2024-10-03 14:51:22 -0700

    Added Task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(2, n + 1):
+        arr = [1]
+        if (len(triangle[-1])) >= 2:
+            arr.extend([triangle[-1][k] + triangle[-1][k + 1] for k in range(len(triangle[-1]) - 1)])
+        arr.append(1)
+        triangle.append(arr)
+    return triangle
+
