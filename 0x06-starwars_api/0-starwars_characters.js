eb47b2bf0c25a1c3fd9d54ff04af516ae1f28915
AhmedShehab1 <legendbtw2003@gmail.com>
2024-11-13 18:24:02 +0000

    Using deprecated request module

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,48 @@
 #!/usr/bin/node
+/* eslint-disable */
+// 0-starwars_characters.js
+const request = require('request');
 
-const movieId = process.argv[2];
+const movieId = process.argv[2]; // The movie ID passed as a positional argument
 
-const printStarWarsCharactersName = async (movieId) => {
+if (!movieId) {
+  console.log('Please provide a movie ID');
+  process.exit(1);
+}
+
+// URL to fetch movie details
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+// Function to fetch JSON data using a URL
+function fetchJson (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        return reject(error);
+      }
+      if (response.statusCode !== 200) {
+        return reject(`Failed to fetch data from ${url}`);
+      }
+      resolve(JSON.parse(body));
+    });
+  });
+}
+
+// Main function to fetch movie data and display character names in order
+async function printCharacterNames () {
   try {
-    let r = await fetch(`https://swapi-api.alx-tools.com/api/films/${movieId}/`); //eslint-disable-line
-    r = await r.json();
-    for (const characterLink of r.characters) {
-      let data = await fetch(characterLink); // eslint-disable-line
-      data = await data.json();
-      console.log(data.name);
-    }
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
-};
+    const movieData = await fetchJson(apiUrl);
+    const characterUrls = movieData.characters;
 
-printStarWarsCharactersName(movieId);
+    // Fetch all character data while preserving order
+    const characterPromises = characterUrls.map(url => fetchJson(url));
+    const characters = await Promise.all(characterPromises);
+
+    // Print each character's name in order
+    characters.forEach(character => console.log(character.name));
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+printCharacterNames();
